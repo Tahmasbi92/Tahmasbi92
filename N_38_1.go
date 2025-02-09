a68a6fb4df593a23ff3aa0712299a2fc936520fb
tahmasbi92 <atemaaaa92@gmail.com>
2025-02-09 22:44:16 +0330

    ایجاد فایل

---
+++ N_38_1.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	mysum := sum(2, 2)
+
+	fmt.Println(mysum)
+
+}
+
+//sum
+
+//dvidid
+
+// ..
+
+func sum(a int, b int) int {
+
+	return a + b
+}
+
+func Minus(a int, b int) int {
+
+	return a - b
+}
+
+func Divided(a int, b int) float32 {
+
+	return float32(a / b)
+}
+
+func Multiplied(a int, b int) int {
+
+	return a * b
+}
