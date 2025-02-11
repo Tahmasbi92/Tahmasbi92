cb13d600b3fc6aa66c9cdfd67d9a2da553d33177
tahmasbi92 <atemaaaa92@gmail.com>
2025-02-11 22:57:18 +0330

    create file

---
+++ N_37_2.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	num1 := 10
+	num2 := 10
+
+	result := num1 + num2
+
+	fmt.Println(result)
+
+	num3 := 20
+	num4 := 30
+
+	result2 := num3 + num4
+
+	fmt.Println(result2)
+
+}
