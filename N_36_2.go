1afebea484782d662b70d66180fca652fb07e18e
tahmasbi92 <atemaaaa92@gmail.com>
2025-02-18 23:21:27 +0330

    create file (Removing an element from a Map in Go)

---
+++ N_36_2.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	sa := map[int]string{1: "alireza", 2: "mohammad", 3: "maraym", 4: "yashar", 5: "mostafa", 6: "masoud"}
+
+	fmt.Println(sa)
+
+	delete(sa, 4)
+
+	fmt.Println(sa)
+
+}
