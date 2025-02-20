92e1b76e5dabb35a9e9f6fbdea4d1312d65e6cd1
tahmasbi92 <atemaaaa92@gmail.com>
2025-02-20 16:37:33 +0330

    create file (Multiple defers together)

--- N_43_3.go
+++ N_43_3.go
@@ -6,5 +6,5 @@ func main() {
 
 	defer fmt.Println("this is first") // خطي که defer قبل از آن آمده است آخر از همه اجرا می شود.
 	fmt.Println("this is second")
-	defer fmt.Println("this is third") // چون در خط اول defer اول آمده است و بعد defer  به خط سوم اضافه شد بنابراین دیرتر از همه اجرا می شود.
+	defer fmt.Println("this is third") // چون در خط اول defer اول آمده و بعد defer  به خط سوم اضافه شد بنابراین دیرتر از همه اجرا می شود.
 }
